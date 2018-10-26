b2869229b794fc50ddff09a0212a671015984767
IT <ledinh.quang1997@gmail.com>
2018-10-26 14:19:13 +0700

    delete import notnull

--- src/main/java/com/codingmanstudio/courses/domain/Category.java
+++ src/main/java/com/codingmanstudio/courses/domain/Category.java
@@ -1,17 +1,14 @@
 package com.codingmanstudio.courses.domain;
 
-import com.codingmanstudio.courses.repository.CourseRepository;
-import com.sun.istack.internal.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 @Getter
 @Setter
